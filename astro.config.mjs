7e8a733dd6a4ae7305c6002b6c5b012b025bba17
Gui <51219838+gui-ace@users.noreply.github.com>
2026-03-14 12:48:10 +0900

    push

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config'
 import UnoCSS from '@unocss/astro'
 import sitemap from '@astrojs/sitemap'
+import rehypeExternalLinks from 'rehype-external-links'
 
 export default defineConfig({
   site: 'https://acecore.net',
   integrations: [UnoCSS({ injectReset: true }), sitemap()],
+  markdown: {
+    rehypePlugins: [
+      [rehypeExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }],
+    ],
+  },
 })
